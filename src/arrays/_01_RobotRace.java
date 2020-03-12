2cfeec9d85029ccddd52ccf09c8a9af300603560
ilabstaff <ilabstaff@172.16.116.71>
2020-03-11 19:24:30 -0700

    kjudhgghjjgh

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,14 +1,36 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
+public static void main(String[] args) {
+	Robot[] robots = new Robot[10];
+	for (int i = 0; i < robots.length; i++) {
+	robots[i]=new Robot();	
+	}
 
 	//2. create an array of 5 robots.
 
 	//3. use a for loop to initialize the robots.
-
-		//4. make each robot start at the bottom of the screen, side by side, facing up
-
+	int x= 50 ;
+	//4. make each robot start at the bottom of the screen, side by side, facing up
+	for (int i = 0; i < robots.length; i++) {
+		robots [i].moveTo(x,550);
+		x+=120;
+	}
+	Random randy = new Random();
+	boolean finished = false;
+	while(!finished) {
+	for (int i = 0; i < robots.length; i++) {
+		robots [i].move(randy.nextInt(51));
+		if(robots[i].getY()<=0) {
+			finished = true ;
+		}
+	}
+	}
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
 
@@ -19,4 +41,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
