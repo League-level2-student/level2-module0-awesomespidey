c157c5c7c7651cd91e577d2380ec408471288a2b
s.weise <s.weise@sw-pc>
2020-03-25 19:28:46 -0700

    asdfgh

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -6,6 +6,7 @@
 
 package arrays;
 
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
@@ -20,9 +21,10 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] buttons;
+	 
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -33,22 +35,28 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String input = JOptionPane.showInputDialog("Give me a  positive number");
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		int num  = Integer.parseInt(input);
+		buttons = new JButton[num];
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
-		
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i] = new JButton();
+			buttons[i].addActionListener(this); 
+			panel.add (buttons[i]);
+
+		}
 		//9 add the panel to the window
-		
+		panel.add (window);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		 Frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+		JFrame.setVisible();
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null,"CLICK ON THE RIGHT BUTTON TO WIN!!!!!!(unless you want to lose(I'm just saying))");
 		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
 		
 		//14. Set the text of the JButton located at hiddenButton to  "ME"
@@ -68,4 +76,4 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		
 		//18. else tell them to try again
 	}
-}
+}                                                                                                                                                          /                                                   /                                                          /                                                    /                                  /
